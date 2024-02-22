e1b5dbf73caa0a698de585c12256fd66b466fa84
Melandry <msmelandry@hotmail.fr>
2024-02-22 15:24:57 +0100

    [REACT] fix project can start now

--- beer-chope-vite/src/App.jsx
+++ beer-chope-vite/src/App.jsx
@@ -3,7 +3,7 @@ import { UserIcon } from "@heroicons/react/24/solid";
 
 function App() {
   return (
-    <div className="bg-slate-400">
+    <div className="bg-purple-500">
       Hello world!
       <UserIcon className="w-4" />
     </div>
