57ab4e1f6b5e4ea155ef8ee009d87d86adb88421
Melandry <msmelandry@hotmail.fr>
2024-02-22 15:13:52 +0100

    [REACT] melandry-dev first init

---
+++ beer-chope-vite/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { UserIcon } from "@heroicons/react/24/solid";
+
+function App() {
+  return (
+    <div className="bg-slate-400">
+      Hello world!
+      <UserIcon className="w-4" />
+    </div>
+  );
+}
+
+export default App;
