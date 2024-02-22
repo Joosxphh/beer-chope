23266fcaf234552e9ab0b031cffc488e3ae650f4
Joosxphh <joseph.maujean@viacesi.fr>
2024-02-22 11:59:20 +0100

    Mise en place des base de données

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,71 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // On génère 50 utilisateurs
+         \App\Models\User::factory(50)->create();
+
+        // On génère 10 fournisseurs
+         \App\Models\Supplier::factory(10)->create();
+
+        // On génère des catégories
+         \App\Models\Category::factory()->create([
+             'name' => 'Blonde',
+             'description' => 'blonde é uma cerveja lager premium',
+             'slug' => 'blonde',
+             'color' => 'yellow',
+         ]);
+
+        \App\Models\Category::factory()->create([
+            'name' => 'Brune',
+            'description' => 'brune é uma cerveja lager premium',
+            'slug' => 'brune',
+            'color' => 'green',
+        ]);
+
+        \App\Models\Category::factory()->create([
+            'name' => 'Rousse',
+            'description' => 'Rousse é uma cerveja lager premium',
+            'slug' => 'rousse',
+            'color' => 'blue',
+        ]);
+
+        \App\Models\Category::factory()->create([
+            'name' => 'Blanche',
+            'description' => 'Blanche é uma cerveja lager premium',
+            'slug' => 'blanche',
+            'color' => 'purple',
+        ]);
+
+        \App\Models\Category::factory()->create([
+            'name' => 'Sans Alcool',
+            'description' => 'Sans Alcool é uma cerveja lager premium',
+            'slug' => 'sans-alcool',
+            'color' => 'orange',
+        ]);
+
+        \App\Models\Category::factory()->create([
+            'name' => 'Rouge',
+            'description' => 'Rouge é uma cerveja lager premium',
+            'slug' => 'rouge',
+            'color' => 'red',
+        ]);
+
+        \App\Models\Category::factory()->create([
+            'name' => 'Triple',
+            'description' => 'Triple é uma cerveja lager premium',
+            'slug' => 'triple',
+            'color' => 'pink',
+        ]);
+
+        $product = \App\Models\Product::factory(100)->create();
+        $categories = \App\Models\Category::all();
+        $product->each(function ($product) use ($categories) {
+            $product->categories()->attach(
+                $categories->random(rand(1,2))->pluck('id')->toArray()
+            );
+        });
+
+
     }
 }
