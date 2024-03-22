ba1a4b48cbad044179133fad2e850da3c6d22b1e
Joosxphh <joseph.maujean@viacesi.fr>
2024-03-22 13:44:10 +0100

    fix: modifs des routes

--- routes/web.php
+++ routes/web.php
@@ -19,20 +19,22 @@ use Illuminate\Support\Facades\Route;
 */
 Route::group(['middleware' => 'admin:web'], function () {
 
-    Route::get('/admin/', function () {
-        return view('dashboard');
+    Route::get('/admin', function () {
+        return redirect()->route('dashboard');
     });
 
     Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 // Routes products
     Route::resource('/admin/product', ProductController::class);
+    Route::get('/product/{id}/restore', [App\Http\Controllers\ProductController::class, 'restore'])->name('product.restore');
 
 // Routes user
     Route::resource('/admin/user', UserController::class);
 
 // Routes order
     Route::resource('/admin/order', OrderController::class);
+    Route::post('/order/{order}/validate', [App\Http\Controllers\OrderController::class, 'validateOrder'])->name('order.validate');
 
 // Logout
     Route::post('admin/logout', [App\Http\Controllers\Auth\LogoutController::class, 'logout'])->name('logout');
