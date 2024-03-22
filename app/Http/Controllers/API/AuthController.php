1117f8d071f0216ccbde0cdd83a709661a3b0eab
Mathis TERNON <mathis.ternon@devinfluence.fr>
2024-03-22 14:35:58 +0100

    Feat : Inscription client

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -18,10 +20,34 @@ class AuthController extends Controller
         if (!Auth::attempt($credentials)) {
             return response()->json(["message" => "Invalid credentials"], 401);
         }
-        //Permet de faire fonctionné le createToken dans VSCode, le com permet de paramétrer le type var $user
+        //Permet de faire fonctionner le createToken dans VSCode, le com permet de paramétrer le type var $user
         /** @var User $user */
         $user = Auth::user();
         $token = $user->createToken("auth_token");
         return ["token" => $token->plainTextToken];
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'address' => 'string|max:255',
+        ]);
+
+        $roleId = $request->input('role_id', 1);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'address' => $request->address,
+            'role_id' => $roleId,
+        ]);
+
+        // Vous pouvez ajouter d'autres fonctionnalités comme l'envoi de courriel de confirmation, etc.
+
+        return response()->json(['message' => 'Utilisateur enregistré avec succès'], 201);
     }
 }
