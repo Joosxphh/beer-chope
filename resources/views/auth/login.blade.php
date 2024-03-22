b8e738146320bbb4d3269f229d1e40113f24c626
Joosxphh <joseph.maujean@viacesi.fr>
2024-03-22 15:18:55 +0100

    feat: création d'un nouveau layout sans sidebar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,24 +1,28 @@
-<x-layout no-sidebar>
-    <x-slot name="title">Connexion</x-slot>
-    <div class="max-w-2xl mx-auto p-8 shadow-md" style="min-height: 92vh;">
-        <h1 class="text-3xl font-bold mb-6">Connexion</h1>
-        <form action="{{ route('login') }}" method="post">
-            @csrf
-            <div class="mb-4">
-                <label for="email" class="block text-gray-600 font-semibold">Email :</label>
-                @error('email')
-                <div class="text-red-500">{{ $message }}</div>
-                @enderror
-                <input type="email" name="email" value="{{ old('email') }}" class="w-full border border-gray-300 p-2 rounded" required>
+<x-layoutFull>
+    <div class="bg-cover bg-center bg-no-repeat h-screen" style="background-image: url('{{ asset('img/background-beer.jpg') }}');">
+        <x-slot name="title">Connexion</x-slot>
+        <div class="flex items-center justify-center h-full">
+            <div class="max-w-4xl mx-auto p-8 shadow-md" style="background-color: rgba(255, 255, 255, 0.9);">
+                <h1 class="text-3xl font-bold mb-6">Connexion</h1>
+                <form action="{{ route('login') }}" method="post">
+                    @csrf
+                    <div class="mb-4">
+                        <label for="email" class="block text-gray-600 font-semibold">Email :</label>
+                        @error('email')
+                        <div class="text-red-500">{{ $message }}</div>
+                        @enderror
+                        <input type="email" name="email" value="{{ old('email') }}" class="w-full border border-gray-300 p-2 rounded" required>
+                    </div>
+                    <div class="mb-4">
+                        <label for="password" class="block text-gray-600 font-semibold">Mot de passe :</label>
+                        @error('password')
+                        <div class="text-red-500">{{ $message }}</div>
+                        @enderror
+                        <input type="password" name="password" class="w-full border border-gray-300 p-2 rounded" required>
+                    </div>
+                    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">Connexion</button>
+                </form>
             </div>
-            <div class="mb-4">
-                <label for="password" class="block text-gray-600 font-semibold">Mot de passe :</label>
-                @error('password')
-                <div class="text-red-500">{{ $message }}</div>
-                @enderror
-                <input type="password" name="password" class="w-full border border-gray-300 p-2 rounded" required>
-            </div>
-            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Connexion</button>
-        </form>
+        </div>
     </div>
-</x-layout>
+</x-layoutFull>
