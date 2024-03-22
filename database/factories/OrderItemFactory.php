205ebdea5b6152f86b8548050e776c049a573a6b
Joosxphh <joseph.maujean@viacesi.fr>
2024-03-22 08:41:47 +0100

    feat: création des factorys

---
+++ database/factories/OrderItemFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\OrderItem>
+ */
+class OrderItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'order_id' => fake()->numberBetween(1, 50),
+            'product_id' => fake()->numberBetween(1, 40),
+            'quantity' => fake()->numberBetween(1, 10),
+            'price' => fake()->randomFloat(2, 1, 100),
+        ];
+    }
+}
